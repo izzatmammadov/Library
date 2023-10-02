937ea16752d14b06314fec51255f8077bae2d8b6
NihatAbasli <nihatabasli@gmail.com>
2023-10-01 23:37:41 -0700

    add aside bar

---
+++ src/js/hamburger.js
@@ -0,0 +1,7 @@
+const sectionLeft = document.querySelector(".left");
+
+const hamburgerOn = document.querySelector("#hamburger");
+
+hamburgerOn.addEventListener("click", function () {
+  sectionLeft.style.display = "block";
+});
