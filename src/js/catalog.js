5466b5eb42fae9d7376315505cc3de30e411f4d6
AzizAzizli <azizliaziz025@gmail.com>
2023-10-03 23:33:31 +0400

    Sliders renewed and design bugs fixed

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -5,48 +5,137 @@ options.forEach((element) => {
     options.forEach((btn) => {
       btn.style.color = "";
     });
-    element.style.color = "orange";
+    element.style.color = "#e16a00";
   });
 });
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  slidesPerView: 1,
-  loop: true,
-  grid: {
-    rows: 1,
-  },
-  breakpoints: {
-    300: {
-      slidesPerView: 1,
-    },
-    640: {
-      slidesPerView: 2,
-    },
-    960: {
-      slidesPerView: 3,
-    },
-    1024: {
-      slidesPerView: 4,
-    },
-  },
+$(document).ready(function () {
+  $(".slider-1").slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: true,
+    prevArrow: $(".prevArrow"),
+    nextArrow: $(".nextArrow"),
+    responsive: [
+      {
+        breakpoint: 1248,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+        },
+      },
+    ],
+  });
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  $(".slider-2").slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
 
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: ".swiper-scrollbar",
-  // },
-  mousewheel: {
-    forceToAxis: true,
-  },
+    prevArrow: $(".prevArrow-bestseller"),
+    nextArrow: $(".nextArrow-bestseller"),
+    responsive: [
+      {
+        breakpoint: 1248,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+  $(".slider-3").slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+
+    prevArrow: $(".prevArrow-new"),
+    nextArrow: $(".nextArrow-new"),
+    responsive: [
+      {
+        breakpoint: 1248,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+        },
+      },
+    ],
+  });
 });
