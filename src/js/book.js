e030ca5caecf17a8cca314208364b38b8f293ec6
izzatmammadov <imamedov388@gmail.com>
2023-10-06 00:32:12 +0400

    contact info add to firebase and fix comments dates

--- src/js/book.js
+++ src/js/book.js
@@ -7,6 +7,7 @@ let bookDetails = document.querySelector(".bookDetails");
 let points = document.querySelector(".points");
 let errorBox = document.querySelector(".errorBox");
 let backBtn = document.querySelector("#backBtn");
+let commentErrorMessage = document.querySelector("#commentErrorMessage");
 
 const date = new Date();
 const hours = date.getHours();
@@ -15,78 +16,75 @@ const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
 
+// For back location
+backBtn.addEventListener("click", function () {
+  window.history.back();
+});
+//
 
-backBtn.addEventListener("click", function(){window.history.back();})
-// Add new comment
-
-
-let commentData = {
-  comments: [], 
-  date: new Date(), 
+const commentData = {
+  comments: [],
+  date: new Date(),
 };
 
-// Add new comment
-commentAdd.addEventListener("click", function (e) {
-  e.preventDefault();
+commentAdd.addEventListener("click", function () {
+  event.preventDefault();
 
-  if (commentInput.value) {
-    errorBox.style.opacity = "0";
-    addComment(commentInput.value);
-  } else {
-    errorBox.style.opacity = "1";
-    errorBox.innerText = "Empty comment";
-    errorBox.style.textAlign = "center";
+  if (commentInput.value == "") {
+    commentErrorMessage.style.display = "block";
+    setTimeout(() => {
+      commentErrorMessage.style.display = "none";
+    }, 1500);
+    return;
   }
+  errorBox.style.opacity = "0";
+  addComment(commentInput.value);
 });
 
 function addComment(commentText) {
+  const timestamp = new Date();
   commentData.comments.push({
     text: commentText,
-    timestamp: new Date(),
+    timestamp: timestamp,
   });
 
   showComments();
-
   commentInput.value = "";
 }
 
-function showComments() {
-  commentData.comments.forEach(function (comment) {
-    let newUserCommentLi = document.createElement(`li`);
-    newUserCommentLi.className = "userCommentLi";
-    newUserCommentLi.innerHTML = `<p><span class="anonim">anonim </span><span>${comment.timestamp.getHours()}:${comment.timestamp.getMinutes()} ${comment.timestamp.getDate()}/${
-    comment.timestamp.getMonth() + 1
-    }/${comment.timestamp.getFullYear()}</span></p><p>${comment.text}</p>`;
+function formatTimestamp(timestamp) {
+  const now = new Date();
+  const diffInDays = Math.floor((now - timestamp) / (24 * 60 * 60 * 1000));
 
+  if (diffInDays === 0) {
+    // Comment was posted today
+    const hours = timestamp.getHours();
+    const minutes = timestamp.getMinutes();
+    return `${hours < 10 ? "0" + hours : hours}:${
+      minutes < 10 ? "0" + minutes : minutes
+    } today`;
+  } else if (diffInDays === 1) {
+    const hours = timestamp.getHours();
+    const minutes = timestamp.getMinutes();
+    return `${hours}:${minutes} yesterday`;
+  } else {
+    const day = timestamp.getDate();
+    const month = timestamp.getMonth() + 1;
+    const year = timestamp.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+}
+
+function showComments() {
+  userCommentUl.innerHTML = "";
+
+  commentData.comments.forEach(function (comment) {
+    let newUserCommentLi = document.createElement("li");
+    newUserCommentLi.className = "userCommentLi";
+    const formattedTimestamp = formatTimestamp(comment.timestamp);
+
+    newUserCommentLi.innerHTML = `<p><span class="anonim">*****</span> <span>${formattedTimestamp}</span></p><p>${comment.text}</p>`;
     userCommentUl.appendChild(newUserCommentLi);
-    userCommentUl.insertBefore(newUserCommentLi, userCommentUl.firstChild);
     userCommentUl.lastElementChild.style.marginBottom = "0";
   });
 }
-
-// More Details
-
-let bookDetailsHeight = bookDetails.style.maxHeight;
-bookDetailsHeight = "262px";
-if (bookDetails.clientHeight < 262) {
-  moreDetails.style.display = "none";
-} else {
-  if (bookDetailsHeight) {
-    moreDetails.textContent = "Show More";
-  } else {
-    moreDetails.textContent = "Show Less";
-  }
-}
-
-moreDetails.addEventListener("click", function () {
-  if (bookDetails.style.maxHeight) {
-    bookDetails.style.maxHeight = null;
-    moreDetails.textContent = "Show More";
-  } else {
-    bookDetails.style.maxHeight = bookDetails.scrollHeight + "px";
-    moreDetails.textContent = "Show Less";
-  }
-});
-console.log(moreDetails.innerText);
-
-console.log();
--- src/js/joinUsModule.js
+++ src/js/joinUsModule.js
@@ -1,60 +1,110 @@
-    // Import the functions you need from the SDKs you need
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-    import {
-      getDatabase,
-      ref,
-      set,
-      push,
-    } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  push,
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-      apiKey: "AIzaSyBjUeKUH3MpDlxQgfX6FM6lZMXNsvR_Ouk",
-      authDomain: "ourproject-e7f72.firebaseapp.com",
-      databaseURL: "https://ourproject-e7f72-default-rtdb.firebaseio.com",
-      projectId: "ourproject-e7f72",
-      storageBucket: "ourproject-e7f72.appspot.com",
-      messagingSenderId: "202148445701",
-      appId: "1:202148445701:web:45845c3f69709c9ec5e66c"
-    };
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const db = getDatabase(app);
-    const users = ref(db, "users");
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjUeKUH3MpDlxQgfX6FM6lZMXNsvR_Ouk",
+  authDomain: "ourproject-e7f72.firebaseapp.com",
+  databaseURL: "https://ourproject-e7f72-default-rtdb.firebaseio.com",
+  projectId: "ourproject-e7f72",
+  storageBucket: "ourproject-e7f72.appspot.com",
+  messagingSenderId: "202148445701",
+  appId: "1:202148445701:web:45845c3f69709c9ec5e66c",
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+// Our collections
+const users = ref(db, "users");
+const contacts = ref(db, "contacts");
 
-    const joinUsFullnameInput = document.querySelector("#joinUsFullname");
-    const joinUsEmailInput = document.querySelector("#joinUsEmail");
-    const joinBtn = document.querySelector("#join");
-    
-    joinBtn.addEventListener("click", function () {
-      const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+// USERS
+const joinUsFullnameInput = document.querySelector("#joinUsFullname");
+const joinUsEmailInput = document.querySelector("#joinUsEmail");
+const joinBtn = document.querySelector("#join");
 
-      if (
-        joinUsFullnameInput.value.trim() === "" ||
-        !emailPattern.test(joinUsEmailInput.value)
-      ) {
-        modalErrorMessage.style.display = "block";
-        setTimeout(() => {
-          modalErrorMessage.style.display = "none";
-        }, 1500);
-        return;
-      }
+joinBtn.addEventListener("click", function () {
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-      const joinUser = {
-        fullname: joinUsFullname.value,
-        email: joinUsEmail.value,
-      };
-      push(users,joinUser)
+  if (
+    joinUsFullnameInput.value.trim() === "" ||
+    !emailPattern.test(joinUsEmailInput.value)
+  ) {
+    modalErrorMessage.style.display = "block";
+    setTimeout(() => {
+      modalErrorMessage.style.display = "none";
+    }, 1500);
+    return;
+  }
 
-      modalSuccessMessage.style.display = "block"
+  const joinUser = {
+    fullname: joinUsFullname.value,
+    email: joinUsEmail.value,
+  };
+  push(users, joinUser);
 
-      setTimeout(() => {
-        modalSuccessMessage.style.display = "none"
-        joinUsFullname.value = "";
-        joinUsEmail.value = "";
-      }, 1500);
+  modalSuccessMessage.style.display = "block";
 
-      // console.log(joinUser); //for add firebase
-    });
+  setTimeout(() => {
+    modalSuccessMessage.style.display = "none";
+    joinUsFullname.value = "";
+    joinUsEmail.value = "";
+  }, 1500);
+
+  // console.log(joinUser); //for add firebase
+});
+
+// CONTACTS
+const contactName = document.querySelector("#contactName");
+const contactEmail = document.querySelector("#contactEmail");
+const contactAdress = document.querySelector("#contactAdress");
+const contactPhone = document.querySelector("#contactPhone");
+const contactTextarea = document.querySelector("#contactTextarea");
+const contactSendBtn = document.querySelector("#contactSendBtn");
+
+contactSendBtn.addEventListener("click", function () {
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (
+    contactName.value.trim() === "" ||
+    contactEmail.value.trim() === "" ||
+    contactAdress.value.trim() === "" ||
+    contactPhone.value.trim() === "" ||
+    contactTextarea.value.trim() === "" ||
+    !emailPattern.test(contactEmail.value)
+  ) {
+    contactErrorMessage.style.display = "block";
+    setTimeout(() => {
+      contactErrorMessage.style.display = "none";
+    }, 1500);
+    return;
+  }
+
+  const contactInfo = {
+    contactName: contactName.value,
+    contactEmail: contactEmail.value,
+    contactAdress: contactAdress.value,
+    contactPhone: contactPhone.value,
+    contactTextarea: contactTextarea.value,
+  };
+  push(contacts, contactInfo)
+
+  contactSuccessMessage.style.display = "block";
+
+  setTimeout(() => {
+    contactSuccessMessage.style.display = "none";
+    contactName.value = "";
+    contactEmail.value = "";
+    contactAdress.value = "";
+    contactPhone.value = "";
+    contactTextarea.value = "";
+  }, 1500);
+});
