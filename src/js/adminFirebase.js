2d681791634426846282545fdea3f93ee48ce86f
AzizAzizli <azizliaziz025@gmail.com>
2023-10-09 17:41:22 +0400

    fixed book date

--- src/js/adminFirebase.js
+++ src/js/adminFirebase.js
@@ -33,7 +33,7 @@ const contacts = ref(db, "contacts");
 
 let date = new Date();
 let addedTime = `${date.getFullYear()}-${
-  date.getMonth()+1 < 10 ? "0" + date.getMonth() : date.getMonth()
+  date.getMonth()+1 < 10 ? "0" + date.getMonth() : date.getMonth()+1
 }-${date.getDay() < 10 ? "0" + date.getDay() : date.getDay()}`;
 console.log(addedTime);
 
